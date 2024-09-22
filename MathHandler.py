190c994b7baa2778f1e7a481c4480a17d2ee8b29
Ahmed-Mahmoud-M <ahmedmahmoud842003@gmail.com>
2024-09-22 18:53:43 +0300

    adding extended ecludin algorithm method

--- MathHandler.py
+++ MathHandler.py
@@ -15,4 +15,39 @@ def square_and_multiply(a,e,n):
     return result
 
 
+#  This function implements the extended ecludin  algorithm for finding inverse multiplication of x 
+# it calculates (e*e^-1) = 1 mod n . it will be used to compute the private key 
+def extended_ecludin_algorithm(e,n):
+    if e>n:
+        a = e
+        b= n
+    else:
+        a = n
+        b = e
+
+    quitont = a // b
+    reminder = a % b
+    t1 = 0
+    t2 = 1
+    t = t1 - (t2*quitont)
+
+    while reminder > 0:
+        a = b
+        b = reminder
+        quitont = int(a//b)
+        reminder = a % b
+        t1 = t2
+        t2 = t
+        t = t1 - (t2*quitont)
+        
+    if t2 < 0 :
+        return round((t2+n))
+
+    else:
+        return round(t2)
+
+
+
+
+
 
