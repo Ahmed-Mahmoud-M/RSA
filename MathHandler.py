04542a64defd7aa433efdae09892599dd1aa54dc
trytogitme <ahmedhasso2003@gmail.com>
2024-09-22 15:49:09 +0300

    created Decryptor and Encryptor classes, capitalized the "main" class, and renamed helper as MathHandler.

--- MathHandler.py
+++ MathHandler.py
@@ -4,15 +4,15 @@
 
 # This function implements the Square and Multiply algorithm for modular exponentiation.
 # It calculates (a^e) mod n efficiently using binary exponentiation.
-def squareAndmultiply(a,e,n):
-    result= 1
+def square_and_multiply(a,e,n):
+    result = 1
     for h in bin(e)[2:]:
-        result = (result**2)% n
+        result = (result**2) % n
         
-        if h == '1' :
-            result = (result*a)% n
+        if h == '1':
+            result = (result*a) % n
 
-    return  result
+    return result
 
 
 
