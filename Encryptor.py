7909b621b1da06c8558ef5cbf72ae44f641e8213
Ahmed-Mahmoud-M <ahmedmahmoud842003@gmail.com>
2024-09-23 19:11:36 +0300

    adding logic of generate_prime

--- Encryptor.py
+++ Encryptor.py
@@ -1,4 +1,5 @@
-
+from MathHandler import miller_Rabin_Primality_Test
+import random
 def encrypt_char():
     return
 
@@ -6,9 +7,22 @@ def encrypt_char():
 def encrypt_str():
     return
 
+# will use it for generating two primes p and q
+def prime_num_generator(digits: int = 3):
+    if digits < 1:
+        raise ValueError("Number of digits must be at least 1.")
+    
+    lower_bound = 10**(digits - 1)
+    upper_bound = 10**digits - 1
+    
+    prime = random.randint(lower_bound, upper_bound)  
+    while not miller_Rabin_Primality_Test(prime, 10):
+        prime = random.randint(lower_bound, upper_bound) 
+    
+    return prime
+    
 
-def prime_num_generator():
-    return
+
 
 
 def public_key_generator():
